cb12f0365c2f099b75f8ee15be6b2d03091ffae7
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-05-26 20:53:57 +0600

    Fixed error

--- Index.js
+++ Index.js
@@ -234,6 +234,7 @@ async function run() {
     app.patch("/deleteBook/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
+      console.log(query);
       const update = {
         $set: {
           availability: true,
